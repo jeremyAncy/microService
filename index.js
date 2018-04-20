95071effa2837eabfcac21a34f7c797d8824726a
POSTEL <richard.postel@ynov.com>
2018-04-20 16:07:18 +0200

    MAJ 2

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ var url = require('url');
 var app = express();
 
 const PORT = 9627;
-const adr = 'http://igg-games.com/';
+var adr = '';
 
 console.log('run server on port : ' + PORT);
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Content-Type', 'application/json');
@@ -21,65 +21,80 @@ app.get('/', function(req, res) {
     res.send(objectTest);
 });
 
-app.get('/image/', function(req, res) {
+app.get('/image/', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Content-Type', 'application/json');
-
-
-    
-    var q = url.parse(adr, true);
-
-    objectTest = {
-        test: 1234
-    }
-
     var json = [];
+    if (req.query.url == null) {
+        var error = true;
+        var description = "error url";
+        var element = { error: error, description: description };
+        json.push(element);
+        return res.send(json);
+    }
+    else
+        adr = req.query.url;
 
+    var q = url.parse(adr, true);
     const options = {
         uri: adr,
         transform: function (body) {
-          return cheerio.load(body);
+            return cheerio.load(body);
         }
-      };
-
-      rp(options)
-      .then(($) => {
-        //console.log($);
-        $('img').each(function(i, elem) {
-            var src = "";
-            var subSrc = $(this).attr('src').trim().substr(0, 4);
-            console.log(subSrc);
-            if(subSrc == "http" || subSrc.indexOf(".com")){
-                src = $(this).attr('src');
+    };
+    rp(options)
+        .then(($) => {
+            //console.log($);
+            $('img').each(function (i, elem) {
                 var src = $(this).attr('src').trim();
-                var alt = $(this).attr('alt');
-                var element = {source: src, alt: alt};
-                json.push(element);
-            } else if (subSrc == "data"){
-                var b64 = true;
-                var src = $(this).attr('src');
-                var alt = $(this).attr('alt');
-                var element = {base64:b64,source: src, alt: alt};
-                json.push(element);
-            } else {
-                console.log($(this).attr('src'));
-            }
-            
-          });
-          res.send(json);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+                var subSrc = $(this).attr('src').trim().substr(0, 4);
+                if (subSrc == "http" || src.indexOf(".com") != -1) {
+                    var alt = $(this).attr('alt');
+                    var element = { source: src, alt: alt };
+                    json.push(element);
+                } else if (subSrc == "data") {
+                    var b64 = true;
+                    var alt = $(this).attr('alt');
+                    var element = { base64: b64, source: src, alt: alt };
+                    json.push(element);
+                } else if(src.indexOf(".jpg") != -1 || src.indexOf(".png") != -1){
+                    q.host;
+                    src = q.host + $(this).attr('src').trim();
+                    var alt = $(this).attr('alt');
+                    var element = { source: src, alt: alt };
+                    json.push(element);
+                }else {
+                    console.log(subSrc);
+                    console.log($(this).attr('src'));
+                }
+            });
+            res.send(json);
+        })
+        .catch((err) => {
+            var error = true;
+            var description = "error url";
+            var element = { error: error, description: description };
+            json.push(element);
+            return res.send(json);
+        });
 
-    
+
 });
 
-app.get('/title/', function(req, res) {
+app.get('/title/', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Content-Type', 'application/json');
+    if (req.query.url == null) {
+        var error = true;
+        var description = "error url";
+        var element = { error: error, description: description };
+        json.push(element);
+        return res.send(json);
+    }
+    else
+        adr = req.query.url;
 
     var q = url.parse(adr, true);
 
@@ -88,25 +103,29 @@ app.get('/title/', function(req, res) {
     const options = {
         uri: adr,
         transform: function (body) {
-          return cheerio.load(body);
+            return cheerio.load(body);
         }
-      };
+    };
 
-      rp(options)
-      .then(($) => {
-        $('title').each(function(i, elem) {
-            var src = q.host;
-            var title = $(this).text();
-            var element = {source: src, title: title};
+    rp(options)
+        .then(($) => {
+            $('title').each(function (i, elem) {
+                var src = q.host;
+                var title = $(this).text();
+                var element = { source: src, title: title };
+                json.push(element);
+            });
+            res.send(json);
+        })
+        .catch((err) => {
+            var error = true;
+            var description = "error url";
+            var element = { error: error, description: description };
             json.push(element);
-          });
-          res.send(json);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+            return res.send(json);
+        });
 
-    
+
 });
 
 app.listen(PORT);
