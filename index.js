c53ef69fbe00e913db03c8c9c06f33f11e91fe43
POSTEL <richard.postel@ynov.com>
2018-04-20 14:45:47 +0200

    MAJ

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+var url = require('url');
 
 var app = express();
 
-const PORT = 8080;
+const PORT = 9627;
+const adr = 'http://igg-games.com/';
 
 console.log('run server on port : ' + PORT);
 
@@ -15,6 +19,94 @@ app.get('/', function(req, res) {
         test: 1234
     }
     res.send(objectTest);
+});
+
+app.get('/image/', function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Content-Type', 'application/json');
+
+
+    
+    var q = url.parse(adr, true);
+
+    objectTest = {
+        test: 1234
+    }
+
+    var json = [];
+
+    const options = {
+        uri: adr,
+        transform: function (body) {
+          return cheerio.load(body);
+        }
+      };
+
+      rp(options)
+      .then(($) => {
+        //console.log($);
+        $('img').each(function(i, elem) {
+            var src = "";
+            var subSrc = $(this).attr('src').trim().substr(0, 4);
+            console.log(subSrc);
+            if(subSrc == "http" || subSrc.indexOf(".com")){
+                src = $(this).attr('src');
+                var src = $(this).attr('src').trim();
+                var alt = $(this).attr('alt');
+                var element = {source: src, alt: alt};
+                json.push(element);
+            } else if (subSrc == "data"){
+                var b64 = true;
+                var src = $(this).attr('src');
+                var alt = $(this).attr('alt');
+                var element = {base64:b64,source: src, alt: alt};
+                json.push(element);
+            } else {
+                console.log($(this).attr('src'));
+            }
+            
+          });
+          res.send(json);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    
+});
+
+app.get('/title/', function(req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Content-Type', 'application/json');
+
+    var q = url.parse(adr, true);
+
+    var json = [];
+
+    const options = {
+        uri: adr,
+        transform: function (body) {
+          return cheerio.load(body);
+        }
+      };
+
+      rp(options)
+      .then(($) => {
+        $('title').each(function(i, elem) {
+            var src = q.host;
+            var title = $(this).text();
+            var element = {source: src, title: title};
+            json.push(element);
+          });
+          res.send(json);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    
 });
 
 app.listen(PORT);
